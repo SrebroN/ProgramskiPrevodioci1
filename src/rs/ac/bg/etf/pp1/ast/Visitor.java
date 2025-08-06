d43a25897b473f3981a03c995f5399934a6c7d6d
Nikola Srebro <srebronikola1@gmail.com>
2025-08-06 15:22:41 +0200

    mjparser

---
+++ src/rs/ac/bg/etf/pp1/ast/Visitor.java
@@ -0,0 +1,12 @@
+// generated with ast extension for cup
+// version 0.8
+// 6/7/2025 13:18:7
+
+
+package src.rs.ac.bg.etf.pp1.ast;
+
+public interface Visitor { 
+
+    public void visit(Program Program);
+
+}
---
+++ src/rs/ac/bg/etf/pp1/ast/VisitorAdaptor.java
@@ -0,0 +1,14 @@
+// generated with ast extension for cup
+// version 0.8
+// 6/7/2025 13:18:7
+
+
+package src.rs.ac.bg.etf.pp1.ast;
+
+public abstract class VisitorAdaptor implements Visitor { 
+
+    public void visit(Program Program) { visit(); }
+
+
+    public void visit() { }
+}
